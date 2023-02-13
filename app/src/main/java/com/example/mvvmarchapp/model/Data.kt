083907685ec316152f778acbe586c67dfa96635a
subhayan.mukhopadhay <subhayan.mukhopadhay@ZMBLR1767.local>
2023-02-13 23:14:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvmarchapp/model/Data.kt
@@ -0,0 +1,5 @@
+package com.example.mvvmarchapp.model
+
+data class Data(
+    val items: List<Item>
+)
---
+++ app/src/main/java/com/example/mvvmarchapp/model/Product.kt
@@ -0,0 +1,7 @@
+package com.example.mvvmarchapp.model
+
+data class Product(
+    val `data`: Data,
+    val error: Any,
+    val status: String
+)
