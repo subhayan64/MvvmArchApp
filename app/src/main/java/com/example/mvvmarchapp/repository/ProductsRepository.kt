3d8cdf95521a80cf633bfe8e30bb90562b30a2a9
Subhayan <subhayan.m5@gmail.com>
2023-02-14 20:51:20 +0530

    added behaviour for network unavailability i.e. display data from db

--- app/src/main/java/com/example/mvvmarchapp/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/mvvmarchapp/repository/ProductsRepository.kt
@@ -3,6 +3,7 @@ package com.example.mvvmarchapp.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.mvvmarchapp.api.ApiInterface
+import com.example.mvvmarchapp.model.Data
 import com.example.mvvmarchapp.model.Product
 import com.example.mvvmarchapp.room.ItemsDatabase
 
@@ -17,15 +18,28 @@ class ProductsRepository(
         get() = productsLiveData
 
     suspend fun getProducts() {
-        val result = apiInterface.getProducts()
-        if (result.body() != null) {
+        ///TODO: check for network connection here
+        if (true) {
+            //when network is available do api call and store data in db
+            val result = apiInterface.getProducts()
+            if (result.body() != null) {
 
-            //to store response data in the db
-            itemsDatabase.itemDao().insertItems(result.body()!!.data.items)
+                //to store response data in the db
+                itemsDatabase.itemDao().insertItems(result.body()!!.data.items)
 
-            //to update the livedata with api response
-            productsLiveData.postValue(result.body())
+                //to update the livedata with api response
+                productsLiveData.postValue(result.body())
+            }
+        } else {
+            //in case of no network: fetch from db
+            val products = itemsDatabase.itemDao().getItems()
+
+            val productsList = Product(Data(products), Unit, "success")
+
+            productsLiveData.postValue(productsList)
         }
+
+
     }
 
 }
