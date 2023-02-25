99c4807cc9a4bac7a3956dd8bdb93b7b96b7c070
Subhayan <subhayan.m5@gmail.com>
2023-02-26 00:51:50 +0530

    add ColumnInfo annotations to item entity

--- app/src/main/java/com/example/mvvmarchapp/model/Item.kt
+++ app/src/main/java/com/example/mvvmarchapp/model/Item.kt
@@ -1,14 +1,19 @@
 package com.example.mvvmarchapp.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.mvvmarchapp.others.Constants
 
 @Entity(tableName = Constants.TABLE_NAME)
 data class Item(
+    @ColumnInfo(name = "extra")
     val extra: String?,
     @PrimaryKey
+    @ColumnInfo(name = "name")
     val name: String,
+    @ColumnInfo(name = "price")
     val price: String,
+    @ColumnInfo(name = "image")
     val image: String
 )
